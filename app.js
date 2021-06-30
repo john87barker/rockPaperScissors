4f57515ef0143a4249636940d8b7e774510c7078
john87barker <john87barker@gmail.com>
2021-06-30 13:56:20 -0600

    some more

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-let playerChoice = [rock, paper, scissors]
+let playerChoice = {
+  rock: {
+
+
+  },
+  paper: {
+
+  },
+  scissors: {
+
+  }
+}
 
 function play(event) {
-
+  if (playerChoice == rock)
+    console.log(rock)
+  if (playerChoice == paper)
+    console.log(paper)
+  if (playerChoice == rock)
+    console.log(paper)
 }
 
 function determineWinner() {
