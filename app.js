12fefa248278407a3f9c63254cebcb203d2a674d
john87barker <john87barker@gmail.com>
2021-06-30 22:41:07 -0600

    basically done

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ function determineWinner(playerWeapon) {
 
 
   if (final == 2) {
-    return document.getElementById('winLose').innerText = `You won with strongest ${playerWeapon} EVER!!! \n you beat ${player[playerWeapon].win}! Nice job!`
+    return document.getElementById('winLose').innerText = `You won with strongest ${playerWeapon} EVER!!! \n You beat ${player[playerWeapon].win} with it! Nice job!`
   }
 
   if (final == 0) {
-    return document.getElementById('winLose').innerText = `Oh no! Your ${playerWeapon} wasn't good enough! \n you lost by ${player[playerWeapon].lose}...that's sad...`
+    return document.getElementById('winLose').innerText = `Oh no! Your ${playerWeapon} wasn't good enough! \n You lost by ${player[playerWeapon].lose}...that's sad...`
 
   } else {
     return document.getElementById('winLose').innerText = `You tied! You both chose ${playerWeapon} you smarty pants!`
